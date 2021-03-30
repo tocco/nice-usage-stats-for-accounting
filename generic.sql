9f45429317f9da939706bbf1dfd20b2b753331f9
Peter Gerber <pgerber@tocco.ch>
2021-03-30 13:57:30 +0200

    Update SQL

--- generic.sql
+++ generic.sql
@@ -8,21 +8,6 @@ SELECT
   -- business units
   (SELECT count(*) FROM nice_business_unit) AS "BUs",
 
-  -- full database size
-  --
-  -- • This includes rows and binaries that have been marked deleted but still uses disk space. Consequently, this
-  --   measurement is very imprecise and prone fluctuation.
-  -- • Size of binaries is included if and only if binaries haven't been migrated to S3.
-  --
-  (SELECT (pg_database_size(current_database())/1024^3))::int AS "database size (GiB)",
-
-  -- size of binaries
-  --
-  -- • Size of Postgres Large Objects or S3 Objects currently referenced.
-  -- • Doesn't include binaries on S3 marked as deleted.
-  --
-  (SELECT (sum(size)/1024^3)::int from _nice_binary) AS "size binaries (GiB)",
-
   -- DMS domains (external)
   (SELECT COUNT(*) FROM nice_domain AS d LEFT OUTER JOIN nice_domain_type AS dt ON d.fk_domain_type = dt.pk WHERE dt.unique_id = 'public_file_repository') AS "DMS domains external",
 
@@ -32,7 +17,7 @@ SELECT
   -- admin users
   --
   -- This includes all users (nice_user) that are linked to an active login with a role of type 'manager. As exception, the
-  -- login 'tocco' is ignored.
+  -- login 'tocco' and logins ending in '@tocco.ch' are ignored.
   --
   (SELECT
      COUNT(DISTINCT u.pk)
@@ -50,6 +35,7 @@ SELECT
      nice_principal_status AS s ON p.fk_principal_status = s.pk
    WHERE
      t.unique_id = 'manager' AND s.unique_id = 'active' AND p.username <> 'tocco'
+       AND p.username NOT LIKE '%@tocco.ch'
   ) AS "admins",
 
   -- outgoing mails per month averaged over the last 12 months (if possible)
